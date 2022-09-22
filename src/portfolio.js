cf3b47ab7083172ab851af32d4622a23938c2258
ivan18gauna <ivan18gauna@gmail.com>
2022-09-22 12:39:03 -0300

    ortografia

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,16 +3,16 @@ import aboutpic from "./components/Access/mePhoto.jpg"
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: '',
-  title: 'JS.',
+  title: 'Mi Portfolio',
 }
 
 const about = {
   photo:aboutpic,
   // all the properties are optional - can be left empty or deleted
-  name: 'Ivan Gauna',
+  name: 'Iván Gauna',
   role: 'Full stack developer',
   description:
-    'Egresado del bootcamp de la academia de soy Henry, estoy altamente interesado en trabajar con personas de las cuales pueda aprender.',
+    'Egresado del bootcamp de la academia, soy Henry, estoy altamente interesado en trabajar donde pueda aportar y aprender de mis compañeros para poder así expandir mis conocimientos y brindar un trabajo de calidad.',
   resume: 'https://example.com',
   social: {
     linkedin: 'http://linkedin.com/in/ivangauna',
@@ -26,7 +26,7 @@ const projects = [
   {
     name: 'Food-App',
     description:
-      'Esta aplicacion fue un proyecto individual donde desarrolle tanto el front-end como el back-end, consumo una API REST donde me traigo todos los datos relevantes de 100 recetas para mostrarlas en el front-end de a 9 recetas gracias al paginado, el ususario podra buscar,filtrar, ordenar y tambien crear una receta.',
+      'Esta aplicación fue un proyecto individual donde desarrolle tanto el Front-end como el Back-end, consumo una API REST donde me traigo todos los datos relevantes de 100 recetas para mostrarlas en el front-end de a 9 recetas gracias al paginado, el usuario podrá buscar, filtrar, ordenar y también crear una receta.',
     stack: ['Javascript', 'HTML', 'Css','React','Redux','Node','Express','Sequelize'],
     sourceCode: 'https://github.com/Ivan18Gauna/PI-Food-main',
     livePreview: 'https://food-app-indol-theta.vercel.app/',
@@ -34,7 +34,7 @@ const projects = [
   {
     name: '+Salud',
     description:
-      'Este es un proyecto grupal donde aprendi a trabajar en equipo usando metodologia agil/scrum, mi mayor participacion fue en el front-end.',
+      'Este es un proyecto grupal donde aprendí a trabajar en equipo usando metodología ágil/scrum, mi mayor participación fue en el Front-end.',
     stack: ['Javascript', 'HTML', 'Css','React','Redux','Node','Express','Sequelize'],
     sourceCode: 'https://github.com/Ivan18Gauna/app-clinica',
     livePreview: 'https://app-clinica.vercel.app/',
@@ -42,10 +42,10 @@ const projects = [
   {
     name: 'Clon de Mercado Libre',
     description:
-      'Este proyecto lo acabo de iniciar por lo cual esta en proceso, la finalidad es aplicar todos mis conocimientos y apreder nuevos.',
+      'Este proyecto lo acabo de iniciar, por lo cual está en proceso, la finalidad es aplicar todos mis conocimientos y aprender nuevos.',
     stack: ['Javascript', 'HTML', 'Css','React','Redux','Node','Express','Sequelize'],
-    sourceCode: 'https://github.com/Ivan18Gauna',
-    livePreview: 'https://www.mercadolibre.com.ar/',
+    sourceCode: '',
+    livePreview: '',
   },
 ]
 
