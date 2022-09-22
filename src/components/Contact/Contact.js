db9512a2fd4307f4327075ca570882f7a592cdc2
ivan18gauna <ivan18gauna@gmail.com>
2022-09-22 13:15:28 -0300

    botones

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,22 +7,25 @@ const Contact = () => {
   return (
     <section className='section contact center' id='contact'>
       <h2 className='section__title'>Contacto</h2>
-      <a href='/'>
+      <div className='redes'>
+      <a href='/' className='a'>
         <span type='button' className='btn btn--outline'>
           Email
         </span>
       </a>
-      <a href='https://github.com/Ivan18Gauna'>
+      <a href='https://github.com/Ivan18Gauna' className='a'>
         <span type='button' className='btn btn--outline'>
           Github
         </span>
       </a>
-      <a href='https://www.linkedin.com/in/ivangauna'>
+      <a href='https://www.linkedin.com/in/ivangauna' className='a'>
         <span type='button' className='btn btn--outline'>
           Linkedin
         </span>
       </a>
+      </div>
     </section>
+    
   )
 }
 
