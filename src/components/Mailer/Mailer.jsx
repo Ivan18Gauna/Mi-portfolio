095ef04da0c6bb7a94a42750f9a9f7d5ac7fc3fd
ivan18gauna <ivan18gauna@gmail.com>
2022-09-23 12:34:15 -0300

    los input limpios

--- src/components/Mailer/Mailer.jsx
+++ src/components/Mailer/Mailer.jsx
@@ -16,43 +16,54 @@ export const Mailer = () => {
   }
   function sendEmail(e) {
     e.preventDefault();
-    if(input.user_name && input.user_email && input.user_message){
-    emailjs
-      .sendForm(
-        "service_jy6a8i4",
-        "template_vcbg65s",
-        e.target,
-        "lVkn881jkfeYEfMvo"
-      )
-      .then((response) => console.log(response))
-      .then((error) => console.log(error));
+    if (input.user_name && input.user_email && input.user_message) {
+      emailjs
+        .sendForm(
+          "service_jy6a8i4",
+          "template_vcbg65s",
+          e.target,
+          "lVkn881jkfeYEfMvo"
+        )
+        .then((response) => console.log(response))
+        .then((error) => console.log(error));
 
       setInput({
         user_name: "",
         user_email: "",
         user_message: "",
       });
-      alert('Su mensaje fu enviado con exito')
-    }else{
-      alert('Todos los campos deven ser completados')
+      alert("Su mensaje fu enviado con exito");
+    } else {
+      alert("Todos los campos deven ser completados");
     }
   }
   return (
     <div className="div-form">
       {/* <h1 className="title-contact">Contactame</h1> */}
       <form className="form-mail" onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" onChange={handleInput} />
+        <label>Nombre</label>
+        <input
+          type="text"
+          name="user_name"
+          onChange={handleInput}
+          value={input.user_name}
+        />
         <hr />
 
         <label>Email</label>
-        <input type="email" name="user_email" onChange={handleInput} />
+        <input
+          type="email"
+          name="user_email"
+          onChange={handleInput}
+          value={input.user_email}
+        />
         <hr />
 
-        <label>Message</label>
+        <label>Mensaje</label>
         <textarea
           name="user_message"
           onChange={handleInput}
+          value={input.user_message}
           className="msj"
           cols="30"
           rows="10"
