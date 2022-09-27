09689a3d5115793abc21a7efa4bc649d5848cc3c
ivan18gauna <ivan18gauna@gmail.com>
2022-09-27 09:48:21 -0300

    cv

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,37 +1,44 @@
-import { contact } from '../../portfolio'
-import './Contact.css'
+import { contact } from "../../portfolio";
+import "./Contact.css";
 
 const Contact = () => {
-  if (!contact.email) return null
+  if (!contact.email) return null;
 
   return (
-    <section className='section contact center' id='contact'>
-      <h2 className='section__title'>Contacto</h2>
-      <div className='redes'>
-      <a href='https://api.whatsapp.com/send?phone=543795022500&text=Hola%20mi%20nombre%20es%20....' target="_blank" className='a'>
-        <span type='button' className='btn btn--outline'>
-        whatsapp
-        </span>
-      </a>
-      <a href='https://github.com/Ivan18Gauna' target="_blank" className='a'>
-        <span type='button' className='btn btn--outline'>
-          Github
-        </span>
-      </a>
-      <a href='https://www.linkedin.com/in/ivangauna' target="_blank" className='a'>
-        <span type='button' className='btn btn--outline'>
-          Linkedin
-        </span>
-      </a>
-      <a href='https://bit.ly/3BBRhgs' target="_blank" className='a'>
-        <span type='button' className='btn btn--outline'>
-        curriculum
-        </span>
-      </a>
-      
+    <section className="section contact center" id="contact">
+      <h2 className="section__title">Contacto</h2>
+      <div className="redes">
+        <a
+          href="https://api.whatsapp.com/send?phone=543795022500&text=Hola%20mi%20nombre%20es%20...."
+          target="_blank"
+          className="a"
+        >
+          <span type="button" className="btn btn--outline">
+            whatsapp
+          </span>
+        </a>
+        <a href="https://github.com/Ivan18Gauna" target="_blank" className="a">
+          <span type="button" className="btn btn--outline">
+            Github
+          </span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ivangauna"
+          target="_blank"
+          className="a"
+        >
+          <span type="button" className="btn btn--outline">
+            Linkedin
+          </span>
+        </a>
+        <a href="https://bit.ly/3BBRhgs" target="_blank" className="a">
+          <span type="button" className="btn btn--outline">
+            curriculum
+          </span>
+        </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
